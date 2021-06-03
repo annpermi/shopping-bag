f5aec50f5cedfdfcc9d8528168c021ae7a0653f8
annpermi <perminova.anna85@gmail.com>
2021-06-03 15:36:49 -0400

    Added material ui

--- src/App.js
+++ src/App.js
@@ -1,60 +1,104 @@
-import React, { useState } from 'react';
-import TodoForm from './components/TodoForm';
-import Todo from './components/Todo';
-import './App.css';
-
+import React, { useState } from "react";
+import {IconButton} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import DoneIcon from '@material-ui/icons/Done';
+import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([
-        {id: 1,
-        text: 'Compile the Spring app for deployment',
-        isCompleted: false},
-        {id: 2,
-        text: 'Open port 8080',
-        isCompleted: false},
-        {id: 3,
-        text: 'SSH into the VM',
-        isCompleted: false}
-  ])
-  // const [editId, setEdit] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+    {
+      id: 1,
+      text: "Compile the Spring app for deployment",
+      isCompleted: false
+    },
+    { id: 2, text: "Open port 8080", isCompleted: false },
+    { id: 3, text: "SSH into the VM", isCompleted: false }
+  ]);
+  const [value, setValue] = useState("");
+  const [isEdited, setIsEdited] = useState(false);
+  const [editedId, setEditedId] = useState(null);
 
-  const editTodo = (id, text) => {
-    const newTodos = todos.filter(todo=>todo.id!==id);
-    let item = todos.find(todo=>todo.id===id);
-    item = {id: id,
-    text: text,}
-    setTodos([...newTodos, item])
-  }
+  const editTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    const editVal = todos.find((todo) => todo.id === id);
+    setEditedId(editVal.id);
+    setValue(editVal.text);
+    setTodos(newTodos);
+    setIsEdited(true);
+  };
 
   const completeTodo = (index) => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
-  }
-  
+  };
+
   const deleteTodo = (index) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
-  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+    setIsEdited(false);
+  };
 
   const addTodo = (value) => {
-    const newTodos = [...todos, { id: todos.length + 1, text: value, isCompleted: false }]
-    setTodos(newTodos)
-  }
+    if (!isEdited) {
+      const newTodos = [
+        ...todos,
+        { id: todos.length + 1, text: value, isCompleted: false }
+      ];
+      setTodos(newTodos);
+    } else {
+      setTodos([...todos, { id: editedId, text: value, isCompleted: false }]);
+    }
+  };
 
-console.log(todos)
-    return (
-      <div className="app">
-        <div className="todo-list">
-          <TodoForm addTodo={addTodo}/>
-          <div>
-            {todos.map((todo, index) => <Todo index={index} key={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} editTodo={editTodo}/>)}
-          </div>
+  return (
+    <div className="app">
+      <div className="todo-list">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            placeholder="Add"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </form>
+        <div>
+          {todos.map((todo, index) => (
+            <div
+              className={todo.isCompleted ? "todo true" : "todo"}
+              key={index}
+            >
+              {todo.text}
+              <div>
+              <IconButton aria-label="complete" color="primary" onClick={() => completeTodo(index)}>
+                <DoneIcon fontSize="small"/>
+              </IconButton>
+              <IconButton aria-label="edit" color="danger" onClick={() => editTodo(todo.id)}>
+                <EditIcon fontSize="small"/>
+              </IconButton>
+              <IconButton aria-label="delete" color="secondary" onClick={() => deleteTodo(index)}>
+                <DeleteIcon fontSize="small"/>
+              </IconButton>
+                {/* <button onClick={() => completeTodo(index)}>Complete</button>
+                <button onClick={() => editTodo(todo.id)}>Edit</button>
+                <button onClick={() => deleteTodo(index)}>X</button> */}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
